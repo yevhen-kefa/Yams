3acd1356653b791c56c27a0076461c405839d914
yevhen-kefa <yevhenkefa@gmail.com>
2025-06-02 19:08:55 +0200

    Limite input

--- src/main/java/yams/controleur/FriendController.java
+++ src/main/java/yams/controleur/FriendController.java
@@ -5,8 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.stage.Stage;
 import yams.model.NavAgent;
+
+import java.util.function.UnaryOperator;
 
 public class FriendController {
     NavAgent nav = new NavAgent();
@@ -19,6 +22,29 @@ public class FriendController {
     private TextField inputFriends;
     @FXML
     private TextField inputBots;
+
+    @FXML
+    void initialize() {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d*")) {
+                try {
+                    if (newText.isEmpty()) return change;
+                    int value = Integer.parseInt(newText);
+                    if (value <= 10) {
+                        return change;
+                    }
+                } catch (NumberFormatException e) {
+                }
+            }
+            return null;
+        };
+
+        inputBots.setTextFormatter(new TextFormatter<>(filter));
+        inputFriends.setTextFormatter(new TextFormatter<>(filter));
+        btnPlay.disableProperty().bind(inputBots.textProperty().isEmpty());
+    }
+
 
     @FXML
     void Return(ActionEvent event) {
